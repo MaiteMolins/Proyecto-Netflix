27fafc56701401484f44534591be38b085ef5276
Your Namegitmgit congig --global user.email maitemolinsc@iqs.url.edugit config --global user.email maitemolinsc@iqs.url.edugit config --global user.name MaiteMolinsgit <you@example.com>
2021-05-31 08:49:41 +0200

    Graphs by country and by date

--- CodiR.R
+++ CodiR.R
@@ -45,7 +45,7 @@ table(netflix$year_added)
 table(netflix$rating)
 
 k <- strsplit(netflix$country, split = ", ")
-netflixP<- data.frame(show_id=rep(netflix$show_id, sapply(k, length)),
+NetflixCountry<- data.frame(show_id=rep(netflix$show_id, sapply(k, length)),
                       type = rep(netflix$type, sapply(k, length)),
                       title = rep(netflix$title, sapply(k, length)),
                       director = rep(netflix$director, sapply(k, length)),
@@ -59,8 +59,8 @@ netflixP<- data.frame(show_id=rep(netflix$show_id, sapply(k, length)),
                       year_added = rep(netflix$year_added, sapply(k, length)),
                       country = unlist(k))
 rm(k)
-netflixP$country<-gsub(",", "", netflixP$country, fixed=TRUE)
-table(netflixP$country, useNA="ifany")
+NetflixCountry$country<-gsub(",", "", NetflixCountry$country, fixed=TRUE)
+table(NetflixCountry$country, useNA="ifany")
 
 k <- strsplit(netflix$listed_in, split = ", ")
 netflixCat<- data.frame(show_id=rep(netflix$show_id, sapply(k, length)),
@@ -79,6 +79,8 @@ netflixCat<- data.frame(show_id=rep(netflix$show_id, sapply(k, length)),
 rm(k)
 table(netflixCat$listed_in, useNA="ifany")
 
+####Number of Movies and TV Shows####
+
 netflixMovies<-netflix[netflix$type=="Movie",]
 netflixMovies$duration<-as.numeric(gsub(" min", "", netflixMovies$duration, fixed=TRUE))
 summary(netflixMovies$duration)
@@ -88,4 +90,43 @@ netflixSeries$duration<-gsub(" Season", "", netflixSeries$duration, fixed=TRUE)
 netflixSeries$duration<-as.numeric(gsub("s", "", netflixSeries$duration, fixed=TRUE))
 summary(netflixSeries$duration)
 
-ggplot(netflix,aes(x=type))+geom_bar(col="black",fill="red")
+ggplot(netflix,aes(x=type))+geom_bar(col="black",fill="red")+  
+  ggtitle(label = "Cantidad de películas y series") + xlab("")+
+  ylab(label="Cantidad total")+theme_classic()+
+  scale_y_continuous(breaks=seq(0,6000,1000))
+
+####Content by Country####
+
+netflixByCountryArr<-NetflixCountry %>% group_by(country, type) %>%
+  summarise(count=n())
+netflixByCountryArr<-reshape(data=data.frame(netflixByCountryArr), idvar="country",
+           v.names="count", timevar="type", direction="wide")
+colnames(netflixByCountryArr)<-c("country", "NumberOfMovies", "NumberOfTVShows")
+netflixByCountryArr[is.na(netflixByCountryArr)]<-0
+netflixByCountryArr<-netflixByCountryArr[-nrow(netflixByCountryArr),]
+netflixByCountryArr$total<-netflixByCountryArr$NumberOfMovies+netflixByCountryArr$NumberOfTVShows
+netflixByCountryArr<-netflixByCountryArr %>% arrange(desc(total))
+netflixByCountryArr<-netflixByCountryArr[netflixByCountryArr$total>=netflixByCountryArr$total[10],]
+ggplot(netflixByCountryArr, aes(NumberOfMovies, NumberOfTVShows, colour=country))+ 
+  geom_point(size=5)+
+  xlab("Number of Movies") + ylab("Number of TV Shows")+
+  ggtitle("Amount of Netflix Content in Top 10 countries")+theme_classic()+
+  labs(col="Country")
+rm(netflixByCountryArr)
+
+####Content by Date####
+netflix$show_id<-tibble(netflix$show_id)
+netflixByDate<-netflix$show_id %>% group_by(netflix$year_added,netflix$type) %>% 
+  summarize(count=n()) %>% na.omit()
+colnames(netflixByDate)<-c("YearAdded", "Type", "count")
+
+ggplot(netflixByDate, aes(YearAdded, count, group=Type))+
+  geom_line(aes(colour = Type), size = 2)+ 
+  geom_point() + 
+  xlab("Year") + 
+  ylab("Number of Content")+
+  ggtitle("Amount of Netflix Content By Time")+
+  theme_classic()
+
+####Content by Rating####
+
